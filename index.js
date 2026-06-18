58fa0e0d0c8274c793e26c5401cf5358840b4186
dhavalnasit33 <dhavalnasit33@gmail.com>
2026-06-18 14:03:21 +0530

    manage rate limit

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.use((req, res, next) => {
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 200,
+  max: 950,
 
   // Skip rate limiting for admin requests (assuming admins send a Bearer token)
   skip: (req, res) => {
